8f3e95284ef197d8724b0151c6195aaf508446fd
Eric Greene <eric@t4d.io>
2020-04-20 11:48:34 -0400

    add code demos

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HelloWorld } from './components/HelloWorld';
+import { CarTool } from './components/CarTool';
 
 ReactDOM.render(
-  // React.createElement(HelloWorld),
-  <HelloWorld />,
+  <>
+    <HelloWorld />
+    <CarTool />
+  </>,
   document.querySelector('#root'),
 );
-
-
 
