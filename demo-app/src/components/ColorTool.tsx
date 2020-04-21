4cf91430e51a913dad9fe2c046c9b2f03482a227
Eric Greene <eric@t4d.io>
2020-04-21 11:49:48 -0400

    add color form

--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 import { Color } from '../models/Color';
 
@@ -6,19 +6,51 @@ export interface ColorToolProps {
   colors: Color[];
 }
 
+interface ColorToolState {
+  name: string;
+  hexcode: string;
+  [ x: string ]: any;
+}
+
 export const ColorTool: FC<ColorToolProps> = (props) => {
+
+  // colorForm represents the data collected on the form
+  // setColorForm is the function which update the form data and re-renders the component
+  const [ colorForm, setColorForm ] = useState({ name: '', hexcode: '' } as ColorToolState);
 
   const colorListItems = props.colors.map( (color) =>
     <li key={color.id}>{color.name}</li>);
 
+  const change = (e: ChangeEvent<HTMLInputElement>) => {
+
+    setColorForm({
+      ...colorForm,
+      [ e.target.name ]: e.target.value,
+    });
+
+  };
+
+  console.log('re-rendering', colorForm);
+
   return (
     <>
-      <header>
+      <header className="page-header">
         <h1>Color Tool</h1>
       </header>
       <ul>
         {colorListItems}
       </ul>
+      <form>
+        <div>
+          {/* React.createElement('label', { htmlFor: 'name-input' }, 'New Color') */}
+          <label htmlFor="name-input">New Color</label>
+          <input type="text" id="name-input" name="name" value={colorForm.name} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="hexcode-input">New Hexcode</label>
+          <input type="text" id="hexcode-input" name="hexcode" value={colorForm.hexcode} onChange={change} />
+        </div>
+      </form>
     </>
   );
 
